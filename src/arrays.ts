0ba0e0b0239646eaa42d4002e8b63000709a30c9
lycheeedotexe <kris082@outlook.com>
2024-02-18 00:04:01 -0500

    tripleNumbers, stringsToIntegers completed; 3/9 done.

--- src/arrays.ts
+++ src/arrays.ts
@@ -19,7 +19,8 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    const tripled = numbers.map((num: number): number => num * 3);
+    return tripled;
 }
 
 /**
@@ -27,7 +28,10 @@ export function tripleNumbers(numbers: number[]): number[] {
  * the number cannot be parsed as an integer, convert it to 0 instead.
  */
 export function stringsToIntegers(numbers: string[]): number[] {
-    return [];
+    const intArr = numbers.map((num: string): number =>
+        Number(num) ? Number(num) : 0
+    );
+    return intArr;
 }
 
 /**

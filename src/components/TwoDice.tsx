333d39f8e6c9a60006d10baee9b3f9e776a5306f
lycheeedotexe <kris082@outlook.com>
2024-03-10 15:56:25 -0400

    finished TwoDice, 5/6

--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -12,5 +12,18 @@ export function d6(): number {
 }
 
 export function TwoDice(): JSX.Element {
-    return <div>Two Dice</div>;
+    const [d1, setD1] = useState<number>(1);
+    const [d2, setD2] = useState<number>(6);
+    return (
+        <div>
+            Left Die: <span data-testid="left-die">{d1}</span> Right Die:{" "}
+            <span data-testid="right-die">{d2}</span>
+            <p>
+                <Button onClick={() => setD1(d6)}>Roll Left</Button>
+                <Button onClick={() => setD2(d6)}>Roll Right</Button>
+            </p>
+            {d1 === d2 && d1 !== 1 && <p>Win</p>}
+            {d1 === 1 && d2 === 1 && <p>Lose</p>}
+        </div>
+    );
 }

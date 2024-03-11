c7680cfa47b4635935e54084d6a0e6b63b1163b9
lycheeedotexe <kris082@outlook.com>
2024-03-10 21:46:43 -0400

    completed CycleHoliday, 6/6; removed html and text.test.tsx

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,42 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+type Holiday = "🧧" | "🎄" | "🎃" | "🤡" | "🥮";
+
+const HOLIDAYS_BY_YEAR: Record<Holiday, Holiday> = {
+    "🧧": "🤡",
+    "🤡": "🥮",
+    "🥮": "🎃",
+    "🎃": "🎄",
+    "🎄": "🧧"
+};
+
+const HOLIDAYS_BY_ABC: Record<Holiday, Holiday> = {
+    "🤡": "🎄",
+    "🎄": "🎃",
+    "🎃": "🧧",
+    "🧧": "🥮",
+    "🥮": "🤡"
+};
+
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [holiday, setHoliday] = useState<Holiday>("🧧");
+
+    function changeHolidayYear(): void {
+        const newHoliday = HOLIDAYS_BY_YEAR[holiday];
+        setHoliday(newHoliday);
+    }
+    function changeHolidayABC(): void {
+        const newHoliday = HOLIDAYS_BY_ABC[holiday];
+        setHoliday(newHoliday);
+    }
+    return (
+        <div>
+            <div>Holiday: {holiday}</div>
+            <div>
+                <Button onClick={changeHolidayABC}>Advance by Alphabet</Button>
+                <Button onClick={changeHolidayYear}>Advance by Year</Button>
+            </div>
+        </div>
+    );
 }
